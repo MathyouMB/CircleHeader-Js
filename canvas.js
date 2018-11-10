07471b2419655474c145f05ad90f4c410f73e8ef
Matthew M-B <matthewmacraebovell@bell.net>
2018-11-09 20:33:01 -0500

    initial push

---
+++ canvas.js
@@ -0,0 +1,32 @@
+var canvas = document.querySelector('canvas')
+	;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+//
+
+//c.fillRect(100,100,100,100);
+//c.stroke();
+
+//console.log(canvas);
+var x = 10//Math.random() * window.innerWidth;
+var y = 10//Math.random() * window.innerHeight;
+function animate(){
+	requestAnimationFrame(animate);
+	c.clearRect(0,0,innerWidth,innerHeight);
+		for(var i = 0; i < 1; i++){
+		
+		c.beginPath();
+		
+		c.arc(x,y,30,0,Math.PI *2,false);
+		c.strokeStyle = 'blue';
+		c.fillRect(x,y,100,100);
+		c.stroke();
+	x += 1;
+	y += 1;
+}
+}
+
+animate();
